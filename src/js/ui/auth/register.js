6a87c0b14989261f39827f76dafbb3d0ce91556c
AnnaSkudsveen <annaskudsveen@hotmail.com>
2024-09-25 13:41:32 +0200

    feat: tried to check linking between modules and add form data to local storage

--- src/js/ui/auth/register.js
+++ src/js/ui/auth/register.js
@@ -1,1 +1,7 @@
-export async function onRegister(event) {}
+// import { register } from "../../api/auth/register";
+
+export async function onRegister(event) {
+  //   console.log(register);
+  console.log("hello");
+  localStorage.setItem("name", `{form.name}`);
+}
