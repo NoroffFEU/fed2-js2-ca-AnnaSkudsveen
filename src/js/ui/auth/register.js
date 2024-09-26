ef51b0a7b3801224af25580c9fa6163af7faee44
AnnaSkudsveen <annaskudsveen@hotmail.com>
2024-09-26 09:18:58 +0200

    feat: use the onRegister function to collect data for register function to send fetch request and register user

--- src/js/ui/auth/register.js
+++ src/js/ui/auth/register.js
@@ -1,7 +1,16 @@
-// import { register } from "../../api/auth/register";
+import { register } from "../../api/auth/register.js";
 
 export async function onRegister(event) {
-  //   console.log(register);
-  console.log("hello");
-  localStorage.setItem("name", `{form.name}`);
+  const form = document.getElementById("registerForm");
+
+  const name = form.name.value;
+  const email = form.email.value;
+  const password = form.password.value;
+  console.log(form);
+  event.preventDefault();
+  console.log("onRegister function");
+
+  register(name, email, password);
+  console.log(name, email, password);
+  // console.log(form.name.value + form.password.value);
 }
