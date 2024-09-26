b6d92fac7728d64910b7f388eb022dfeb0f2ffce
AnnaSkudsveen <annaskudsveen@hotmail.com>
2024-09-26 20:49:37 +0200

    chore: cleaned up code

--- src/js/router/views/post.js
+++ src/js/router/views/post.js
@@ -1,38 +1,9 @@
-import { API_KEY, API_SOCIAL_POSTS } from "../../api/constants.js";
+import { readPost } from "../../api/post/read.js";
 
-const bearerToken = localStorage.getItem("bearerToken");
-const postSection = document.querySelector(".postSection");
 const url = new URL(window.location.href);
 const params = new URLSearchParams(url.search);
 const postId = params.get("id");
 
+readPost(postId);
+
 console.log(postId);
-
-if (bearerToken) {
-  const options = {
-    headers: {
-      Authorization: `Bearer ${bearerToken}`,
-      "X-Noroff-API-Key": `${API_KEY}`
-    }
-  };
-
-  const response = await fetch(`${API_SOCIAL_POSTS}/${postId}`, options);
-  const data = await response.json();
-  console.log(`${API_SOCIAL_POSTS}/${postId}`);
-  console.log(data);
-  showPost(data.data);
-} else {
-  authGuard();
-}
-
-function showPost(postData) {
-  postSection.innerHTML += `
-      <section class="blog-post">
-      <div>
-      <h2>${postData.title}</h2>
-      <p>${postData.body}</p>
-      </div>
-      <button>Read more</button>
-      </section>
-        `;
-}
