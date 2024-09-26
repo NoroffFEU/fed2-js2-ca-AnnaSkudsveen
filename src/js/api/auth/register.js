a30c5d4ba1b25ae7926b6c6d41d10f08a98edf2d
AnnaSkudsveen <annaskudsveen@hotmail.com>
2024-09-26 09:17:46 +0200

    feat: added the fetch request to the register function

--- src/js/api/auth/register.js
+++ src/js/api/auth/register.js
@@ -1,4 +1,39 @@
-export async function register({ name, email, password }) {}
-// bio,
-// banner,
-// avatar,
+import { API_AUTH_LOGIN, API_AUTH_REGISTER } from "../constants.js";
+
+export async function register(name, email, password) {
+  console.log("Received parameters:" + name + email + password);
+  console.log("Register function");
+  console.log(API_AUTH_REGISTER);
+  console.log(`name: ${name}, email: ${email}, password: ${password}`);
+  try {
+    fetch(`https://v2.api.noroff.dev/auth/register`, {
+      method: "POST",
+      body: JSON.stringify({
+        name: `${name}`,
+        email: `${email}`,
+        password: `${password}`,
+      }),
+
+      // header taken from: https://mollify.noroff.dev/content/feu1/javascript-1/module-5/api-methods/http-post-request-method?nav=programme
+      headers: {
+        "Content-type": "application/json; charset=UTF-8",
+      },
+    })
+      .then((response) => {
+        response.json();
+        console.log(response);
+        if (!response.ok) {
+          alert("Error: try again");
+        } else {
+          alert("New user created");
+          window.location.replace("/auth/login/index.html");
+        }
+      })
+      .then((data) => {
+        console.log("data: " + data);
+      });
+  } catch (error) {
+    console.error("Error during registration:", error);
+    throw error; // Rethrow the error to handle it in `onRegister`
+  }
+}
