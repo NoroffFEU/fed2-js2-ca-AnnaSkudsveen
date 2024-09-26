50a235d4218b76a677668c2a7e2fcae611d6fb06
AnnaSkudsveen <annaskudsveen@hotmail.com>
2024-09-26 14:00:34 +0200

    feat: fetch request to post email and password and recieve an accesstoken

--- src/js/api/auth/login.js
+++ src/js/api/auth/login.js
@@ -1,1 +1,33 @@
-export async function login({ email, password }) {}
+import { API_AUTH_LOGIN } from "../constants.js";
+
+export async function login(email, password) {
+  try {
+    fetch(`${API_AUTH_LOGIN}`, {
+      method: "POST",
+      body: JSON.stringify({
+        email: `${email}`,
+        password: `${password}`
+      }),
+
+      // header taken from: https://mollify.noroff.dev/content/feu1/javascript-1/module-5/api-methods/http-post-request-method?nav=programme
+      headers: {
+        "Content-type": "application/json; charset=UTF-8"
+      }
+    })
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        console.log("data: ", data);
+        if (data.data && data.data.accessToken) {
+          localStorage.setItem("bearerToken", data.data.accessToken);
+          window.location.href = "/index.html";
+        } else {
+          console.error("Access token not found in response");
+        }
+      });
+  } catch (error) {
+    console.error("Error during login:", error);
+    throw error; // Rethrow the error to handle it in `onRegister`
+  }
+}
