1545f78c115b89f80e18ce0693892cdb3802a561
AnnaSkudsveen <annaskudsveen@hotmail.com>
2024-09-26 19:10:55 +0200

    feat: fetch all posts and console log them

--- src/js/router/views/home.js
+++ src/js/router/views/home.js
@@ -1,3 +1,22 @@
 import { authGuard } from "../../utilities/authGuard";
+import { API_KEY, API_SOCIAL_POSTS } from "../../api/constants.js";
+console.log("hellooo");
 
-authGuard();
+const bearerToken = localStorage.getItem("bearerToken");
+console.log(bearerToken);
+
+if (bearerToken) {
+  const options = {
+    headers: {
+      Authorization: `Bearer ${bearerToken}`,
+      "X-Noroff-API-Key": `${API_KEY}`
+    }
+  };
+
+  const response = await fetch(`${API_SOCIAL_POSTS}`, options);
+  const data = await response.json();
+
+  console.log(data);
+} else {
+  authGuard();
+}
