b70ff334d7d342bbf221ef9d9d9579ab3f96f894
AnnaSkudsveen <annaskudsveen@hotmail.com>
2024-09-26 23:23:39 +0200

    feat: added function to createPost

--- src/js/api/post/create.js
+++ src/js/api/post/create.js
@@ -1,1 +1,32 @@
-export async function createPost({ title, body, tags, media }) {}
+import { API_SOCIAL_POSTS, API_KEY } from "../constants.js";
+
+const bearerToken = localStorage.getItem("bearerToken");
+
+export async function createPost(title, body) {
+  try {
+    fetch(`${API_SOCIAL_POSTS}`, {
+      method: "POST",
+      body: JSON.stringify({
+        title: `${title}`,
+        body: `${body}`
+        // media: `${media}`
+      }),
+
+      headers: {
+        "Content-type": "application/json; charset=UTF-8",
+        "Authorization": `Bearer ${bearerToken}`,
+        "X-Noroff-API-Key": `${API_KEY}`
+      }
+    })
+      .then((response) => {
+        console.log(response);
+        return response.json();
+      })
+      .then((data) => {
+        console.log("post data: ", data);
+      });
+  } catch (error) {
+    console.error("Error during posting:", error);
+    throw error; // Rethrow the error to handle it in `onRegister`
+  }
+}
