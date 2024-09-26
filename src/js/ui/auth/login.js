f33e94ce4eb54d141243e722eec13fa2e17040ef
AnnaSkudsveen <annaskudsveen@hotmail.com>
2024-09-26 14:02:04 +0200

    feat: use onlogin function to handle submit event and pass info to login function

--- src/js/ui/auth/login.js
+++ src/js/ui/auth/login.js
@@ -1,1 +1,12 @@
-export async function onLogin(event) {}
+import { login } from "../../api/auth/login.js";
+
+export async function onLogin(event) {
+  event.preventDefault();
+
+  const form = document.forms.login;
+
+  const email = form.email.value;
+  const password = form.password.value;
+
+  login(email, password);
+}
