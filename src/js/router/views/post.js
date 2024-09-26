7a66a30fcb7f78c173b0c3bd43c6580bfa3a4000
AnnaSkudsveen <annaskudsveen@hotmail.com>
2024-09-26 20:44:13 +0200

    feat: show single post when clicking link

--- src/js/router/views/post.js
+++ src/js/router/views/post.js
@@ -1,1 +1,38 @@
-alert("Single Post Page");
+import { API_KEY, API_SOCIAL_POSTS } from "../../api/constants.js";
+
+const bearerToken = localStorage.getItem("bearerToken");
+const postSection = document.querySelector(".postSection");
+const url = new URL(window.location.href);
+const params = new URLSearchParams(url.search);
+const postId = params.get("id");
+
+console.log(postId);
+
+if (bearerToken) {
+  const options = {
+    headers: {
+      Authorization: `Bearer ${bearerToken}`,
+      "X-Noroff-API-Key": `${API_KEY}`
+    }
+  };
+
+  const response = await fetch(`${API_SOCIAL_POSTS}/${postId}`, options);
+  const data = await response.json();
+  console.log(`${API_SOCIAL_POSTS}/${postId}`);
+  console.log(data);
+  showPost(data.data);
+} else {
+  authGuard();
+}
+
+function showPost(postData) {
+  postSection.innerHTML += `
+      <section class="blog-post">
+      <div>
+      <h2>${postData.title}</h2>
+      <p>${postData.body}</p>
+      </div>
+      <button>Read more</button>
+      </section>
+        `;
+}
