802e6cde256ec469d3e88311f66852ece86d2c1f
AnnaSkudsveen <annaskudsveen@hotmail.com>
2024-09-26 23:23:25 +0200

    feat: added function to onCreatePost

--- src/js/ui/post/create.js
+++ src/js/ui/post/create.js
@@ -1,1 +1,14 @@
-export async function onCreatePost(event) {}
+import { createPost } from "../../api/post/create.js";
+
+export async function onCreatePost(event) {
+  event.preventDefault();
+  const form = document.forms.createPost;
+  console.log(form);
+  const name = form.name.value;
+  const body = form.body.value;
+  //   const media = form.media.value;
+
+  console.log(`name: ${name}, body ${body}`);
+
+  createPost(name, body);
+}
