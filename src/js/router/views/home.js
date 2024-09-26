87e3766531693e1a1514706646ccd0710e8d9021
AnnaSkudsveen <annaskudsveen@hotmail.com>
2024-09-26 19:34:35 +0200

    feat: show posts on homepage

--- src/js/router/views/home.js
+++ src/js/router/views/home.js
@@ -1,9 +1,8 @@
 import { authGuard } from "../../utilities/authGuard";
 import { API_KEY, API_SOCIAL_POSTS } from "../../api/constants.js";
-console.log("hellooo");
 
 const bearerToken = localStorage.getItem("bearerToken");
-console.log(bearerToken);
+const postSection = document.querySelector(".postSection");
 
 if (bearerToken) {
   const options = {
@@ -16,7 +15,22 @@ if (bearerToken) {
   const response = await fetch(`${API_SOCIAL_POSTS}`, options);
   const data = await response.json();
 
-  console.log(data);
+  showPosts(data.data);
 } else {
   authGuard();
+}
+
+function showPosts(postData) {
+  postData.forEach((post) => {
+    postSection.innerHTML += `
+      <a class="post-link-card" href="post/index.html?id=${post.id}">
+      <section class="blog-post">
+      <div>
+      <h2>${post.title}</h2>
+      </div>
+      <button>Read more</button>
+      </section>
+      </a>
+        `;
+  });
 }
