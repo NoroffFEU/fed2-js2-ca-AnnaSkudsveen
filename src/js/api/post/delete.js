5380b774dcfd5e30ed51dbcd8cbe734926b5b5ea
AnnaSkudsveen <annaskudsveen@hotmail.com>
2024-09-27 14:59:03 +0200

    Feat: Added delete function

--- src/js/api/post/delete.js
+++ src/js/api/post/delete.js
@@ -1,1 +1,33 @@
-export async function deletePost(id) {}
+import { API_SOCIAL_POSTS, API_KEY } from "../constants.js";
+
+const bearerToken = localStorage.getItem("bearerToken");
+
+export async function deletePost(id) {
+  try {
+    fetch(`${API_SOCIAL_POSTS}/${id}`, {
+      method: "DELETE",
+      body: JSON.stringify({
+        id: `${id}`
+      }),
+
+      headers: {
+        "Content-type": "application/json; charset=UTF-8",
+        "Authorization": `Bearer ${bearerToken}`,
+        "X-Noroff-API-Key": `${API_KEY}`
+      }
+    })
+      .then((response) => {
+        console.log(response);
+        if (!response.ok) {
+          alert("Error during deleting, post was not deleted");
+        }
+        window.location.href = "/index.html";
+      })
+      .then((data) => {
+        window.location.href = "/index.html";
+      });
+  } catch (error) {
+    console.error("Error during deleting:", error);
+    throw error;
+  }
+}
