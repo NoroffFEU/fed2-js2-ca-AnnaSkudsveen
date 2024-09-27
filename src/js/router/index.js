06e4e3a6dec1d6ab1627691924bc2856e7e3ab53
AnnaSkudsveen <annaskudsveen@hotmail.com>
2024-09-27 10:20:28 +0200

    fix: fixed paths so they would work if they ended in index.html also

--- src/js/router/index.js
+++ src/js/router/index.js
@@ -15,18 +15,23 @@ export default async function router(pathname = window.location.pathname) {
       await import("./views/login.js");
       break;
     case "/auth/register/":
+    case "/auth/register/index.html":
       await import("./views/register.js");
       break;
+    case "/post/":
     case "/post/index.html":
       await import("./views/post.js");
       break;
     case "/post/edit/":
+    case "/post/edit/index.html":
       await import("./views/postEdit.js");
       break;
     case "/post/create/":
+    case "/post/create/index.html":
       await import("./views/postCreate.js");
       break;
     case "/profile/":
+    case "/profile/index.html":
       await import("./views/profile.js");
       break;
     default:
